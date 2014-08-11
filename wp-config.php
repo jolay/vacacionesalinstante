e8f780295c8be8353589ef22ac647c843e03d965
jolay <jolay@intelisis.com>
2014-08-11 04:23:59 +0000

    update db3

--- wp-config.php
+++ wp-config.php
@@ -13,8 +13,6 @@
  *
  * @package WordPress
  */
-define('WP_HOME','http://localhost/vacacionesalinstante/');
-define('WP_SITEURL','http://localhost/vacacionesalinstante/');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'vacacionesdb');
